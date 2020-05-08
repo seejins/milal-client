ebaf639787e58c657558b398dbc82c5b4c0f7db1
Sejin Hwang <sejinhwang91@gmail.com>
2020-05-08 15:15:22 -0700

    added landing page

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -6,7 +6,11 @@ class LandingPage extends Component {
         return (
             <>
             <div className='landing-page container'>
-                <h1> Hello World</h1>
+                <div className='landing-page description'>
+                    <h1> Hello World</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tincidunt purus at turpis tincidunt, a mattis lectus commodo. Donec neque enim, lobortis in velit vitae, dictum dapibus mauris. Nullam sit amet ligula suscipit, semper libero ut, placerat urna. Proin eleifend in magna ac interdum. Nunc non purus neque. Vivamus venenatis, ex non consectetur hendrerit, mauris sem sodales purus, vel posuere velit orci ac arcu.</p>
+                </div>
+                
             </div>
             </>
         )
